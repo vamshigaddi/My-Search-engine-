e04f7392847e1867fc8cb60db95142e5531b6a51
vamshi gaddi <144581498+vamshigaddi@users.noreply.github.com>
2024-03-17 16:25:57 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
-SEARCH_KEY = "AIzaSyC5-4WLDS8Ww53XYUZp6oeb8FsA-cXmpyY"
-SEARCH_ID = "b2925ac52333c4ec2"
+SEARCH_KEY = ""
+SEARCH_ID = " "
 COUNTRY = "IND"
 SEARCH_URL = "https://www.googleapis.com/customsearch/v1?key={key}&cx={cx}&q={query}&start={start}&num=10&gl=" + COUNTRY
 RESULT_COUNT = 15
